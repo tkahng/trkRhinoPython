956f1289b1dc62347bbf7c5324bccc4ab18a9c76
tkahng <tkahng@gmail.com>
2020-05-06 08:45:00 +0900

    a

--- sortbyposition_gh.py
+++ sortbyposition_gh.py
@@ -1,11 +1,19 @@
 import rhinoscriptsyntax as rs
 import ghpythonlib.treehelpers as th
 import scriptcontext as sc
-# import Rhino
+import Rhino as rc
+import System.Guid as Guid
+# import Grasshopper as gh
 
-
+# sc.doc = rc.RhinoDoc.ActiveDoc
+# # rc.RhinoDoc.Objects.
+doc = Rhino.RhinoDoc.ActiveDoc
+# rc.RhinoDoc.SelectObjects
+# rs.object
+print(rs.coercerhinoobject(guids[0]))
 print(type(srfs[0]))
 print(srfs[0])
+print(rs.coerceguid(srfs[0]))
 
 seq = [x * dist + dist for x in range(count)]
 intervals = [(x - dist, x) for x in seq]
@@ -17,7 +25,7 @@ def groupByPosition(pairs):
     return sorted(newpairs, key=lambda x:x[0][1])
 
 srfpairs = [[x, rs.SurfaceAreaCentroid(x)[0][axis], str(x)] for x in srfs]
-
+guidout = [rs.coercerhinoobject(x) for x in guids]
 print(srfpairs[0][0])
 
 # grouped = groupByPosition(srfpairs)
@@ -27,8 +35,26 @@ print(srfpairs[0][0])
 b = th.list_to_tree(groupByPosition(srfpairs))
 
 a = srfs
+# b = guidout
 """[summary]
 """
+
+
+# import rhinoscriptsyntax as rs
+# import System.Guid as Guid
+
+# import Rhino
+
+
+# doc = Rhino.RhinoDoc.ActiveDoc
+
+# gid = Guid.Parse(x)
+
+# obj = doc.Objects.Find(gid)
+# geo = rs.coercegeometry(gid)
+# layer = doc.Layers[obj.Attributes.LayerIndex].Name
+# a = doc.Layers[obj.Attributes.LayerIndex].Name
+# b = geo
 
 # from ghpythonlib.componentbase import executingcomponent as component
 # import Grasshopper, GhPython
