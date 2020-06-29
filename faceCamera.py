8ea45e80a8862d62eb216d30d051a54659517a31
tkahng <tkahng@gmail.com>
2020-06-29 23:50:22 +0900

    a

--- faceCamera.py
+++ faceCamera.py
@@ -1,13 +1,32 @@
 import rhinoscriptsyntax as rs
-import trkRhinoPy as trp
+import scriptcontext as sc
+# import Rhino as rc
 
+# sc.doc = rc.RhinoDoc.ActiveDoc
 objs = rs.GetObjects("Select polysurface to explode", rs.filter.polysurface|rs.filter.mesh|rs.filter.instance, preselect=True)
 
-cameraPos = rs.ViewCamera()
+
+def blkFace(obj):
+
+    cameraPos = rs.ViewCamera()
+
+    cameraPos.Z = 0
+
+    xform = rs.BlockInstanceXform(obj)
+    plane = rs.PlaneTransform(rs.WorldXYPlane(), xform)
+
+    viewdir = rs.VectorUnitize(cameraPos - plane.Origin)
+
+    angle = rs.VectorAngle(viewdir, plane.YAxis)
+
+    newXform = rs.XformRotation3(plane.YAxis, viewdir, plane.Origin)
+
+    # newplane = rs.RotatePlane(plane, angle, plane.ZAxis)
+
+    # newXform = rs.XformChangeBasis(plane, newplane)
+
+    rs.TransformObject(obj, newXform)
+
+map(blkFace, objs)
 
 
-def faceSrf(obj):
-    srfDir = trp.getSrfNormal(obj)
-
-def faceBlk(obj):
-    sourceXform = rs.BlockInstanceXform(source)
--- gh/ghfacecamera.py
+++ gh/ghfacecamera.py
@@ -14,12 +14,10 @@ cameraPos.Z = 0
 xform = rs.BlockInstanceXform(x)
 plane = rs.PlaneTransform(rs.WorldXYPlane(), xform)
 
-viewdir = cameraPos - plane.Origin
-#viewdir = plane.Origin - cameraPos
+viewdir = rs.VectorUnitize(cameraPos - plane.Origin)
 
-
-#angle = rs.VectorAngle(plane.YAxis, viewdir)
 angle = rs.VectorAngle(viewdir, plane.YAxis)
+
 
 newplane = rs.RotatePlane(plane, angle, plane.ZAxis)
 
