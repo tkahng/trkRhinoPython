3854469db93f545dc159c549b7f9d145f64b50c9
tkahng <tkahng@gmail.com>
2020-08-07 07:58:01 +0900

    a

--- wallsfrombreps.py
+++ wallsfrombreps.py
@@ -9,28 +9,32 @@ tol=sc.doc.ModelAbsoluteTolerance
 trans=rg.CurveOffsetCornerStyle.Sharp
 
 objs = rs.GetObjects('select polysrfs', rs.filter.polysurface, preselect=True)
+wallthickness = rs.GetReal('wall thickness', number=200)
 
-def func(objid):
+
+def makeWalls(objid):
+    height = float(rs.GetUserText(objid, 'height'))
     obj = sc.doc.Objects.Find(objid).Geometry
     facecrvs = []
     curves = obj.DuplicateNakedEdgeCurves(1, 1)
     curves = rg.Curve.JoinCurves(curves, tol)
-    curves = curves[0].Offset(rs.WorldXYPlane(),2,tol,trans)
+    curves = curves[0].Offset(rs.WorldXYPlane(),wallthickness/2,tol,trans)
     facecrvs.append(curves[0])
     brepfaces=[face.DuplicateFace(False) for face in obj.Faces]
     for x in brepfaces:
         crvs = x.DuplicateEdgeCurves()
         crvs = rg.Curve.JoinCurves(crvs, tol)
-        crvs = crvs[0].Offset(rs.WorldXYPlane(),-1,tol,trans)
+        crvs = crvs[0].Offset(rs.WorldXYPlane(),-wallthickness/2,tol,trans)
     #    pln = rs.CurvePlane(crvID)
         
         facecrvs.append(crvs[0])
     plnrsrf = rg.Brep.CreatePlanarBreps(facecrvs, tol)
-    rails = [rg.LineCurve(srf.Faces[0].PointAt(0,0), srf.Faces[0].PointAt(0,0) + rg.Point3d(0,0,4)) for srf in plnrsrf]
-    solids = [rg.BrepFace.CreateExtrusion(srf.Faces[0], rg.LineCurve(srf.Faces[0].PointAt(0,0), srf.Faces[0].PointAt(0,0) + rg.Point3d(0,0,4)), True) for srf in plnrsrf]
+    # rails = [rg.LineCurve(srf.Faces[0].PointAt(0,0), srf.Faces[0].PointAt(0,0) + rg.Point3d(0,0,height)) for srf in plnrsrf]
+    solids = [rg.BrepFace.CreateExtrusion(srf.Faces[0], rg.LineCurve(srf.Faces[0].PointAt(0,0), srf.Faces[0].PointAt(0,0) + rg.Point3d(0,0,height)), True) for srf in plnrsrf]
     solidsrc = [sc.doc.Objects.AddBrep(c) for c in solids]
-    railsrc = [sc.doc.Objects.AddCurve(c) for c in rails]
-    rc = [sc.doc.Objects.AddBrep(c) for c in plnrsrf]
-    return rc
+    # railsrc = [sc.doc.Objects.AddCurve(c) for c in rails]
+    # facecrvsrc = [sc.doc.Objects.AddCurve(c) for c in facecrvs]
+    # rc = [sc.doc.Objects.AddBrep(c) for c in plnrsrf]
+    return solidsrc
 
-map(func, objs)
+map(makeWalls, objs)
