e311ba9da67a9340d9c7d38fa96644cd787fbde1
tkahng <tkahng@gmail.com>
2020-11-03 00:31:40 +0900

    a

--- gh/ghBakeBlkXform.py
+++ gh/ghBakeBlkXform.py
@@ -4,41 +4,66 @@
         y: The y script variable
     Output:
         a: The a output variable"""
-
+        
 __author__ = "인시공현장PC"
 __version__ = "2020.08.09"
-
+        
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
 import Rhino
-
+import ghpythonlib.treehelpers as th
+        
 sc.doc = Rhino.RhinoDoc.ActiveDoc
 
-def bakeblk(blkname, xform, bakename, idx):
-    bakedblk = rs.InsertBlock2(blkname, xform)
-    rs.SetUserText(bakedblk, bakename, idx)
+def maptree(input):
 
-def reset(bakename):
-    comm = ('SelKey', bakename, '_Delete')
-    s = ' '
-    comstr = s.join(comm)
-    print comstr
-    rs.Command(comstr)
+    kvs = []
+    for i in range(input.BranchCount):
+        path = input.Path(i)
+        data = list(input.Branch(path))
+        for idx, val in enumerate(data):
+            param = '{}({})'.format(str(path), str(idx))
+            kv = dict(path=param, obj=val)
+            kvs.append(kv)
+    return kvs
 
-def func(xforms):
-    namestr = ('BakeName(', bakeName, ')')
-    sep = ''
-    bakename = sep.join(namestr)
-#    bakename = bakeName
-    reset(bakename)
-    for idx, xf in enumerate(xforms, start=1):
-        bakeblk(name, xf, bakename, idx)
-#    map(lambda x, y: bakeblk(name, x, bakename, y), xforms, idxs)
+def func(name, xform, bakeName):
+    objpair = maptree(xform)
+    
+    idef = sc.doc.InstanceDefinitions.Find(name)
+    idefx = idef.Index
+    
+    bakename = 'BakeName({})'.format(bakeName)
+    
+    objtbl = sc.doc.Objects
+    
+    existing = []
+    
+    for rhObj in objtbl:
+        try:
+            rawAtts = rhObj.Attributes.GetUserStrings()
+            keys = rawAtts.AllKeys
+            if bakename in keys:
+                existing.append(rhObj)
+        except:
+            pass
 
-
+    for rhObj in existing:
+        try:
+            objtbl.Delete(rhObj, False)
+        except:
+            pass
+    
+    newblk = []
+    
+    for kv in objpair:
+        try:
+            attr = Rhino.DocObjects.ObjectAttributes()
+            attr.SetUserString(bakename, kv['path'])
+            
+            newblk.append(objtbl.AddInstanceObject(idefx, kv['obj'], attr))
+        except:
+            pass
 
 if bake is True:
-    func(xform)
-#
-#if reset is True:
-#    reset()
+    func(name, xform, bakeName)
