dfa1bfe2277eae3b537cecd165eae2a0ad17b4c9
tkahng <tkahng@gmail.com>
2020-11-25 00:22:40 +0900

    a

--- test.py
+++ test.py
@@ -4,22 +4,47 @@
 # from Rhino import *
 # import Rhino
 import Rhino
-from Rhino import *
+#from Rhino import *
 
-Rhino.Geometry
+#Rhino.Geometry
 # import Rhino
 
 # Rhino.Geometry.
 
 
 # import Rhino
-# import rhinoscriptsyntax as rs
-# import scriptcontext as sc
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
 # import trkRhinoPy as trp
 # # import Rhino.Geometry
 
+ids = rs.GetObjects("select polysrf", rs.filter.polysurface|rs.filter.surface, preselect=True)
 
-# objid = rs.GetObject("select polysrf", rs.filter.polysurface, preselect=True)
+rs.EnableRedraw(False)
+
+breps = [rs.coercegeometry(id) for id in ids]
+
+sils = []
+
+for brep in breps:
+    edges = brep.Edges
+    edgecrvs = [e.DuplicateCurve() for e in edges]
+    sils.extend(edgecrvs)
+print sils
+
+silscrv = [sc.doc.Objects.AddCurve(sil) for sil in sils]
+rs.UnselectAllObjects()
+rs.SelectObjects(silscrv)
+rs.Command('_CurveBoolean AllRegions _Enter')
+rs.DeleteObjects(silscrv)
+
+
+#flats = [rg.Curve.ProjectToPlane(e, rs.WorldXYPlane()) for e in edgecrvs]
+#print flats
+#a = flats
+#
+#crvunion = rg.Curve.CreateBooleanUnion(flats, Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance)
+#print list(crvunion)
 
 # brep = rs.coercebrep(objid)
 
